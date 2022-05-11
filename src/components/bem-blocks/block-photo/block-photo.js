b80ab8b67b7f84fe73f5e57b409dd07193b023bb
Andrey Molodyk <87598011+AndrewMolodyk@users.noreply.github.com>
2022-05-11 17:46:09 +0700

    порабол со стилями, доработал кнопку 'показать еще фото'

--- src/components/bem-blocks/block-photo/block-photo.js
+++ src/components/bem-blocks/block-photo/block-photo.js
@@ -1,17 +1,16 @@
 window.onload = function() {
-    let photo = document.getElementsByClassName('photo-gallery');
-    let btn = document.getElementById('photo-gallery-btn');
+    let photo = document.getElementsByClassName('photo-gallery photo-hidden');
+    let moreBtn = document.getElementById('photo-gallery-btn');
     for (let i = 8; i < photo.length; i++) {
         photo[i].style.display = "none";
     }
 
     let count = 8;
-    btn.addEventListener("click", function() {
-        count += 8;
-        let i = 0;
-        if (count <= photo.length) {
-            for (i = 0; i < count; i++) {
-                photo[i].style.display = "block";
+    moreBtn.addEventListener("click", function() {
+        if (count < photo.length) {
+            count = photo.length;
+            for (let y = 0; y < count; y++) {
+                photo[y].style.display = "block";
             }
         }
     })
